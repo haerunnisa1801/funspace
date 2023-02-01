ea02ed2d288035440157ea8c948b0f9c2e1ae9ea
haerunnisa1801 <hnisa9567@gmail.com>
2023-02-01 13:53:03 +0800

    luas persegi panjang

--- app.py
+++ app.py
@@ -4,6 +4,16 @@ def luas_segitiga():
     a = int(input("Masukkan Alas Segitiga"))
     t = int(input("Masukkan Tinggi Segitiga"))
     luas = a * t/2
-    print("Luas Segitiga Adalah: ", lulas)
+    print("Luas Segitiga Adalah: " , luas)
 
 luas_segitiga()
+
+# Hitung Luas Persegi Panjang
+
+def luas_persegi_panjang():
+    p = int(input("Masukkan Panjang Persegi"))
+    l = int(input("Masukkan Lebar Persegi"))
+    luas = p * l
+    print("Luas Persegi Adalah: " , luas)
+
+luas_persegi_panjang()
