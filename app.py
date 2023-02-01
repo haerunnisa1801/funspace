d783413ab2c2c91703f65c0c95d0e3bec935fd3a
haerunnisa1801 <hnisa9567@gmail.com>
2023-02-01 13:24:52 +0800

    upload bagian pertama

---
+++ app.py
@@ -0,0 +1,9 @@
+# Hitung Luas Segitiga
+
+def luas_segitiga():
+    a = int(input("Masukkan Alas Segitiga"))
+    t = int(input("Masukkan Tinggi Segitiga"))
+    luas = a * t/2
+    print("Luas Segitiga Adalah: ", lulas)
+
+luas_segitiga()
