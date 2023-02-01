f673ef500921c8b54939b159417d5c18c1783df9
haerunnisa1801 <123912918+haerunnisa1801@users.noreply.github.com>
2023-02-01 14:06:00 +0800

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,19 @@
+# Hitung Luas Segitiga
+
+def luas_segitiga():
+    a = int(input("Masukkan Alas Segitiga"))
+    t = int(input("Masukkan Tinggi Segitiga"))
+    luas = a * t/2
+    print("Luas Segitiga Adalah: " , luas)
+
+luas_segitiga()
+
+# Hitung Luas Persegi Panjang
+
+def luas_persegi_panjang():
+    p = int(input("Masukkan Panjang Persegi"))
+    l = int(input("Masukkan Lebar Persegi"))
+    luas = p * l
+    print("Luas Persegi Adalah: " , luas)
+
+luas_persegi_panjang()
